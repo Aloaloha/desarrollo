bbd12357a4b8e1e90ef7c72f20defa1fa448bb80
Santiago Carmona López <santiagocarmonalopez@gmail.com>
2015-04-13 01:32:07 +0200

    Version 1.0.8

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -3,7 +3,7 @@
 
 $this->pageTitle=Yii::app()->name;
 ?>  
-
+  
 <div class="carousel slide" id="miSlider" data-ride="carousel">
            
           <ol class="carousel-indicators">
@@ -12,35 +12,36 @@ $this->pageTitle=Yii::app()->name;
             <li data-target="#miSlider" data-slide-to="2"></li>
           </ol>
 
-          <div class="carousel-inner">
+          <div class="carousel-inner"> 
 
-            <div class="item active">
+              <div class="item active">
               <a href="#miSlider">
-                <img src="img/imagen1.jpg" alt="Imagen1">
+                <?php echo "<img src=\".." ?><?php echo Yii::app()->theme->baseUrl; ?><?php echo"/img/img1.jpg\" >";?>
                 <div class="carousel-caption">
-                  <h1>Sala</h1>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, asperiores</p>
+                  <h1>Relax</h1>
+                  <p>Alojamientos pensados para ti!</p>
                 </div>
               </a>
             </div> 
 
             <div class="item">
-              <img src="img/imagen2.jpg" alt="Imagen2">
+            <?php echo "<img src=\".." ?><?php echo Yii::app()->theme->baseUrl; ?><?php echo"/img/img2.jpg\" >";?>
+
               <div class="carousel-caption">
-                <h1>Comedor</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, asperiores</p>
+                <h1>Granada, lugar de encuentro.</h1>
+                <p>Adentrate en su Alcaicería, sumergete en las noches del Albayzin</p>
               </div>
             </div>
 
             <div class="item">
-              <img src="img/imagen3.jpg" alt="Imagen3">
+            <?php echo "<img src=\".." ?><?php echo Yii::app()->theme->baseUrl; ?><?php echo"/img/img3.jpg\" >";?>
               <div class="carousel-caption">
                 <div class="row">
                   <div class="col-xs-12 col-sm-12">
-                    <h1>Perro</h1>
+                    <h1>Sierra Nevada</h1>
                   </div>
                   <div class="hidden-xs col-sm-12">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, numquam, ut voluptatibus fuga porro quae cupiditate aperiam beatae mollitia maxime libero explicabo molestias in saepe ab aliquid culpa accusantium. Illo!</p>
+                    <p>Disfruta de tu deporte favorito, más de 60km de pistas abiertas, te están esperando.</p>
                   </div>
                 </div>
               </div>
@@ -54,3 +55,4 @@ $this->pageTitle=Yii::app()->name;
 
         </div>
 
+ 
